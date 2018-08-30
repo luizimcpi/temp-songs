9f2bcc7c2eaba2702d4a006703038fd9377f5d16
Luiz Henrique <luiz.hse@hotmail.com>
2018-08-30 14:08:13 -0300

    Altera uri da api de clima

--- src/main/java/com/devlhse/tempsongs/service/CityService.java
+++ src/main/java/com/devlhse/tempsongs/service/CityService.java
@@ -9,6 +9,6 @@ public class CityService {
 
     public City getCityInfoByName(String cityName){
         RestTemplate template = new RestTemplate();
-        return template.getForObject("https://samples.openweathermap.org/data/2.5/weather?q={cityName}&appid=130aab12fceacb0e3c68009f1e07b0ea", City.class, cityName);
+        return template.getForObject("http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid=130aab12fceacb0e3c68009f1e07b0ea", City.class, cityName);
     }
 }
