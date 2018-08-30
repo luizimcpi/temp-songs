45f81ae66bd85c38c0a2bf063c8ac75ad97dd516
Luiz Henrique <luiz.hse@hotmail.com>
2018-08-30 00:06:42 -0300

    Remove Feign Client e deixa restTemplate

--- src/main/java/com/devlhse/tempsongs/controller/CityController.java
+++ src/main/java/com/devlhse/tempsongs/controller/CityController.java
@@ -1,7 +1,7 @@
 package com.devlhse.tempsongs.controller;
 
 import com.devlhse.tempsongs.dto.City;
-import com.devlhse.tempsongs.service.CityClient;
+import com.devlhse.tempsongs.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CityController {
 
     @Autowired
-    private CityClient cityClient;
+    private CityService cityService;
 
     @RequestMapping(value = "/{cityName}/songs", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<City> getCityInfoByName(@PathVariable("cityName") String cityName){
-        City city = cityClient.getCityInfoByName(cityName);
+        City city = cityService.getCityInfoByName(cityName);
         return ResponseEntity.ok(city);
     }
 }
