74ec2e4010a629ffd30da42e82d7697a2d6adda7
Luiz Henrique <luiz.hse@hotmail.com>
2018-08-29 21:49:16 -0300

    adiciona estrutura inicial do projeto

---
+++ src/main/java/com/devlhse/tempsongs/TempSongsApplication.java
@@ -0,0 +1,12 @@
+package com.devlhse.tempsongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempSongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempSongsApplication.class, args);
+	}
+}
